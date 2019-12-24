8204de411ee926fe81f0c2c930082a4fa16e2f68
张林锋 <zhanglinfeng@cvte.com>
2019-12-24 16:28:37 +0800

    first init

---
+++ src/index.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import './index.less';
+
+export interface BanPressImgProps {
+  src: any
+  onClick?: () => void
+  className: string
+  alt?: string
+}
+
+export default class BanPressImg extends React.Component<BanPressImgProps> {
+  private img;
+  private mask;
+
+  componentDidMount() {
+    this.img.addEventListener('touchstart', e => {
+      e.preventDefault();
+    });
+    this.img.addEventListener('load', () => {
+      const width = this.img.offsetWidth;
+      const height = this.img.offsetHeight;
+      const left = this.img.offsetLeft;
+      const top = this.img.offsetTop;
+      this.mask.style.width = `${width}px`;
+      this.mask.style.height = `${height}px`;
+      this.mask.style.left = `${left}px`;
+      this.mask.style.top = `${top}px`;
+      this.mask.style.position = `absolute`;
+      this.mask.className = this.img.className.replace('ban-press-mask', '');
+    });
+  }
+
+  public render() {
+    const {src, className, onClick, alt} = this.props;
+    return (
+      <React.Fragment>
+        <img alt={alt} onClick={onClick} ref={ele => this.img = ele} src={src} className={`ban-long-press ${className}`} />
+        <div onClick={onClick} ref={ele => this.mask = ele} className="ban-press-mask" />
+      </React.Fragment>
+    );
+  }
+}
